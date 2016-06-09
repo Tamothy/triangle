3243d6856f0c241cadde11793011c6d0fe88279f
Cecilia Le Brech and Tam Nguyen <mr.tamron@gmail.com>
2016-06-08 19:37:40 -0700

    add backend/frontend logic to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
 // Business (or back-end) logic:
-
+var triangle = function(a, b, c) {
+  var result;
+  if (a ===  b && a === c && b === c) {
+    result = "Equilateral";
+  } else if (a === b || a === c || b === c) {
+    result = "Isosceles";
+  } else if (a !== b && a !== c && b !== c) {
+    result = "Scalene";
+  }
+  return result;
+};
 
 // User interface (or front-end) logic
+$(function() {
+  $("form#triangle").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var number3 = parseInt($("#input3").val());
+    var inputs = triangle(number1, number2, number3);
+    $("#output").text(inputs);
+  });
+});
